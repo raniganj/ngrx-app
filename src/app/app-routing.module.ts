c48a0aefa2306136b63290c1573a66f4ba9a24c1
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-24 06:27:36 +0530

    routing starts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './component/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users',
+    component: UserComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
