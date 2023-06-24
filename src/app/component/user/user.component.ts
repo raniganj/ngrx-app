1e70d521f69516d3e9fc81c0012feaa473303632
sunilmondal4you <sunilmondal4you@gmail.com>
2023-06-24 12:48:37 +0530

    user state update

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { MyAppState } from 'src/app/store/root.reducer';
+import { UserData1, userData1Increament, userData1IncreamentByParams } from 'src/app/store/user.reducer';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userData: UserData1 | undefined;
+  inputValue:number = 0;
+
+  constructor(private store:Store<MyAppState>) { }
 
   ngOnInit(): void {
+    this.store.subscribe((state)=>{
+      this.userData = state.userData1;
+    })
   }
 
+  userData1Increament(){
+    this.store.dispatch(userData1Increament());
+  }
+
+  userData1IncreamentByParams(){
+    this.store.dispatch(userData1IncreamentByParams({val : this.inputValue}))
+  }
 }
--- src/app/store/root.reducer.ts
+++ src/app/store/root.reducer.ts
@@ -1,10 +1,13 @@
 import { counterReducer } from "./counter.reducer"
+import { UserData1, userData1Reducer } from "./user.reducer";
 
 export interface MyAppState {
-  counter : number
+  counter : number;
+  userData1 : UserData1
 }
 
 
 export const rootReducer = {
-   counter : counterReducer
+   counter : counterReducer,
+   userData1 : userData1Reducer
 }
